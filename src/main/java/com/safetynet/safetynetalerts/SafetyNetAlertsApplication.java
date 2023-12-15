cc7bf0627b40e23b05db75b1cedebfb5c280a438
Bastien <bastien.brgt@protonmail.com>
2023-12-15 18:13:41 +0100

    Initialization of the project.

---
+++ src/main/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetalerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/safetynetalerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
