66251cfd6b5768d3ccfaa3d9a8a0d1fa8c95fa71
Bastien <bastien.brgt@protonmail.com>
2023-12-22 13:48:13 +0100

    Update responses to JSON format

--- src/main/java/com/safetynet/safetynetalerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/MedicalRecordController.java
@@ -1,6 +1,7 @@
 package com.safetynet.safetynetalerts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,19 +20,19 @@ public class MedicalRecordController {
 	@Autowired
 	private MedicalRecordService medicalRecordService;
 
-	@PostMapping("/medicalRecord/{firstName}/{lastName}")
+	@PostMapping(value = "/medicalRecord/{firstName}/{lastName}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public MedicalRecord postMedicalRecord(@PathVariable("firstName") String firstName,
 			@PathVariable("lastName") String lastName, @Valid @RequestBody MedicalRecord medicalRecord) {
 		return medicalRecordService.postMedicalRecord(firstName, lastName, medicalRecord);
 	}
 
-	@PutMapping("/medicalRecord/{firstName}/{lastName}")
+	@PutMapping(value = "/medicalRecord/{firstName}/{lastName}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public MedicalRecord putMedicalRecord(@PathVariable("firstName") String firstName,
 			@PathVariable("lastName") String lastName, @Valid @RequestBody MedicalRecord medicalRecord) {
 		return medicalRecordService.putMedicalRecord(firstName, lastName, medicalRecord);
 	}
 
-	@DeleteMapping("/medicalRecord/{firstName}/{lastName}")
+	@DeleteMapping(value = "/medicalRecord/{firstName}/{lastName}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String deleteMedicalRecord(@PathVariable("firstName") String firstName,
 			@PathVariable("lastName") String lastName) {
 		return medicalRecordService.deleteMedicalRecord(firstName, lastName);
