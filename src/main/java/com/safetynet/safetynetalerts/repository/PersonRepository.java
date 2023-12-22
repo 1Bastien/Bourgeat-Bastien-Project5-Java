d97702dc35738076faf65094302ceb8255bd0ffe
Bastien <bastien.brgt@protonmail.com>
2023-12-22 13:50:11 +0100

    Add method to retrieve Persons by Address and by City

--- src/main/java/com/safetynet/safetynetalerts/repository/PersonRepository.java
+++ src/main/java/com/safetynet/safetynetalerts/repository/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.safetynet.safetynetalerts.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -11,4 +12,8 @@ import com.safetynet.safetynetalerts.model.Person;
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
 	Optional<Person> findByFirstNameAndLastName(String firstName, String lastName);
+
+	List<Person> findByAddress(String address);
+
+	List<Person> findByCity(String city);
 }
